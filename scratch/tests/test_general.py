d030c87fe61d369f22be3505a5121f5b200d4c2e
Jev <jev.kuznetsov@gmail.com>
2025-05-20 12:30:59 +0200

    update tests

--- scratch/tests/test_general.py
+++ scratch/tests/test_general.py
@@ -2,14 +2,17 @@
 General functionality tests
 """
 
+import scipy.stats
+import filterpy.stats
+import numpy as np
 from pytest import approx
 
 
 def test_pdf():
     """compare filterpy to sklearn.stats"""
-    from scipy.stats import norm
-    import filterpy.stats
 
-    a = norm(2, 3).pdf(1.5)
+    # NOTE: this works with scalars, but not with arrays
+
+    a = scipy.stats.norm(2, 3).pdf(1.5)
     b = filterpy.stats.gaussian(x=1.5, mean=2, var=3 * 3)
     assert a == approx(b)
